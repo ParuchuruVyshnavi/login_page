9c1902dfb0652aea23b35e3b3bff2cc23a122749
Paruchuru Vyshnavi <paruchuruvyshnavi87@gmail.com>
2026-05-07 11:35:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// LOGIN
+
 function login() {
   auth.signInWithEmailAndPassword(
     email.value, password.value
@@ -7,7 +7,7 @@ function login() {
   }).catch(err => alert(err.message));
 }
 
-// SIGNUP
+
 function signup() {
   auth.createUserWithEmailAndPassword(
     email.value, password.value
@@ -16,7 +16,7 @@ function signup() {
   }).catch(err => alert(err.message));
 }
 
-// GOOGLE LOGIN
+
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(provider)
@@ -24,14 +24,14 @@ function googleLogin() {
     .catch(err => alert(err.message));
 }
 
-// FORGOT PASSWORD
+
 function resetPassword() {
   auth.sendPasswordResetEmail(email.value)
     .then(() => alert("Reset email sent"))
     .catch(err => alert(err.message));
 }
 
-// LOGOUT
+
 function logout() {
   auth.signOut().then(() => {
     window.location.href = "index.html";
